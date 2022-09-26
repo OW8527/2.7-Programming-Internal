201c44e1cd8f2dbe68b146be69195e49bcda0c9e
Oliver Walsh <82142081+OW8527@users.noreply.github.com>
2022-09-26 22:28:20 +0000

    Completed the 5 boxes in terms of positioning and making them a combobox (drop down menu) now I need to code and link up the combobox to the excel sheet to get the right values

--- main.py
+++ main.py
@@ -8,7 +8,37 @@ root = tk.Tk()
 # config the root window
 root.geometry('600x400')
 root.resizable(False, False)
-root.title('Combobox Widget')
+root.title('Buget Laptop Calculator')
+
+# purpose needed------------------------------------------------------------------------------------------
+
+# label
+label = ttk.Label(text="Please select the main reason or purpose of the laptop:")
+label.pack(fill=tk.X, padx=5, pady=5)
+
+# create a combobox
+purpose = tk.StringVar()
+purpose_cb = ttk.Combobox(root, textvariable=purpose)
+
+# get first 3 letters of every month name
+purpose_cb['values'] = [month_name[m][0:3] for m in range(1, 13)]
+
+# prevent typing a value
+purpose_cb['state'] = 'readonly'
+
+# place the widget
+purpose_cb.pack(fill=tk.X, padx=5, pady=5)
+
+
+'''
+# bind the selected value changes
+def month_changed(event):
+    """ handle the month changed event """
+    showinfo(title='Result', message=f'You selected {selected_month.get()}!')
+
+
+month_cb.bind('<<ComboboxSelected>>', month_changed)
+'''
 
 #operating system ---------------------------------------------
 
@@ -43,7 +73,7 @@ month_cb.bind('<<ComboboxSelected>>', month_changed)
 #brand--------------------------------------------------
 
 # label
-brand = ttk.Label(text="Please select a month:")
+brand = ttk.Label(text="Please select a prefered brand for your laptop:")
 brand.pack(fill=tk.X, padx=5, pady=5)
 
 # create a combobox
@@ -71,21 +101,21 @@ month_cb.bind('<<ComboboxSelected>>', month_changed)
 #ram--------------------------------------------------------------------
 
 # label
-label = ttk.Label(text="Please select a month:")
+label = ttk.Label(text="Please select the amount of ram you would like for your laptop:")
 label.pack(fill=tk.X, padx=5, pady=5)
 
 # create a combobox
-selected_month = tk.StringVar()
-month_cb = ttk.Combobox(root, textvariable=selected_month)
+selected_label = tk.StringVar()
+label_cb = ttk.Combobox(root, textvariable=selected_label)
 
 # get first 3 letters of every month name
-month_cb['values'] = [month_name[m][0:3] for m in range(1, 13)]
+label_cb['values'] = [month_name[m][0:3] for m in range(1, 13)]
 
 # prevent typing a value
-month_cb['state'] = 'readonly'
+label_cb['state'] = 'readonly'
 
 # place the widget
-month_cb.pack(fill=tk.X, padx=5, pady=5)
+label_cb.pack(fill=tk.X, padx=5, pady=5)
 
 '''
 # bind the selected value changes
@@ -99,21 +129,21 @@ month_cb.bind('<<ComboboxSelected>>', month_changed)
 #storage-----------------------------------------------------------
 
 # label
-label = ttk.Label(text="Please select a month:")
-label.pack(fill=tk.X, padx=5, pady=5)
+storage = ttk.Label(text="Please select the amount of storage you would like for your laptop:")
+storage.pack(fill=tk.X, padx=5, pady=5)
 
 # create a combobox
-selected_month = tk.StringVar()
-month_cb = ttk.Combobox(root, textvariable=selected_month)
+selected_storage = tk.StringVar()
+selcected_brand_cb = ttk.Combobox(root, textvariable=selected_storage)
 
 # get first 3 letters of every month name
-month_cb['values'] = [month_name[m][0:3] for m in range(1, 13)]
+selcected_brand_cb['values'] = [month_name[m][0:3] for m in range(1, 13)]
 
 # prevent typing a value
-month_cb['state'] = 'readonly'
+selcected_brand_cb['state'] = 'readonly'
 
 # place the widget
-month_cb.pack(fill=tk.X, padx=5, pady=5)
+selcected_brand_cb.pack(fill=tk.X, padx=5, pady=5)
 
 '''
 # bind the selected value changes
